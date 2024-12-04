f78071706d9c4d04a507090ac75267695bde4faf
ironichniyyy <ironichniyyy@gmail.com>
2024-12-04 16:07:32 +0300

    db.go now contains easy code, whic prints 'Hello world!' if connection to db is successful

--- postgresql/db.go
+++ postgresql/db.go
@@ -8,11 +8,20 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func main(){
-	urlExample := "postgres://admin:admin@localhost:5432/finances_db"
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+func main() {
+	conn, err := pgx.Connect(context.Background(), "postgres://admin:admin@localhost:5432/finances_db")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
+	defer conn.Close(context.Background())
+
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
 }
