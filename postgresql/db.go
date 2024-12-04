ccc21d05116fd74a394a6eb4ececdeffbe86c284
ironichniyyy <ironichniyyy@gmail.com>
2024-12-04 17:41:05 +0300

    Added goose and some other changes

--- postgresql/db.go
+++ postgresql/db.go
@@ -1,27 +1,11 @@
+
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-
-	"github.com/jackc/pgx/v5"
+    "database/sql"
+    "fmt"
+    "github.com/lib/pq"
 )
-
 func main() {
-	conn, err := pgx.Connect(context.Background(), "postgres://admin:admin@localhost:5432/finances_db")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.Background())
-
-	var greeting string
-	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println(greeting)
+	
 }
