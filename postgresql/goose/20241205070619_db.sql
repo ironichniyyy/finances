e89a24bd4e193122874cf8afe3810e113d7074bb
ironichniyyy <ironichniyyy@gmail.com>
2024-12-06 13:52:19 +0300

    Added goose migration

--- postgresql/goose/20241205070619_db.sql
+++ postgresql/goose/20241205070619_db.sql
@@ -1,9 +1,9 @@
--- +goose Up
--- +goose StatementBegin
-SELECT 'up SQL query';
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-SELECT 'down SQL query';
--- +goose StatementEnd
+-- +goose up
+CREATE TABLE operations (
+    id SERIAL PRIMARY KEY,               -- Уникальный идентификатор операции
+    operation_time TIMESTAMP NOT NULL,   -- Время операции
+    amount NUMERIC(10, 2) NOT NULL,      -- Стоимость операции (положительная или отрицательная)
+    operation_name VARCHAR(255) NOT NULL -- Название операции
+);
+-- +goose down
+DROP TABLE operations;
